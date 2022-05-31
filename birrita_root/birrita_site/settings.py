6b657c439cd996b70b8aeebc6fa4dd93939b1c1d
eflipe <felipecabaleiro@gmail.com>
2022-05-31 11:50:09 -0300

    [BIRR-06] fixed errors

--- birrita_root/birrita_site/settings.py
+++ birrita_root/birrita_site/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False or env('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['birrita-app.herokuapp.com', 'localhost']
 
